a8d307b2ebfe6045e9eeafc0d27a4d6d9d17a9f6
Edwin-Alomoto <Edwin_Alomoto@outlook.com>
2023-08-18 10:49:57 -0500

    correcion de los puntos y comas

--- Ejercisio git/Ejercisio.js
+++ Ejercisio git/Ejercisio.js
@@ -1,5 +1,5 @@
 const saludar = function(){
-    let cmpSaludo = document.getElementById("imgSaludo")
-    cmpSaludo.src = ("./Imagenes/hacer-venta.gif")
+    let cmpSaludo = document.getElementById("imgSaludo");
+    cmpSaludo.src = ("./Imagenes/hacer-venta.gif");
     console.log("ingresa a saludar");
 }
