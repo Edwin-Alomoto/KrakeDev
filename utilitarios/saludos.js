9ca56a82823636cd452783aea1ff4f02edd5dc76
Edwin-Alomoto <Edwin_Alomoto@outlook.com>
2023-08-18 15:41:56 -0500

    Sacar funciones a otros archivos implementada

--- utilitarios/saludos.js
+++ utilitarios/saludos.js
@@ -9,35 +9,3 @@ saludar= function(){
     mostrarImagen("ImgSaludos","./imagenes/saludos.gif");
     mostrarTextoEnCaja("txtNombre", " ");
 }
-
-mostrarImagen = function(idComponente, rutaImagen){
-    let componente = document.getElementById(idComponente);
-    componente.src = rutaImagen;
-}
-
-mostrarTextoEnCaja = function(idComponente, mensaje){
-    let componente = document.getElementById(idComponente);
-    componente.value=mensaje;
-}
-mostrarTexto = function(idComponente, mensaje){
-    let componente = document.getElementById(idComponente);
-    componente.innerText=mensaje;
-}
-
-recuperarTexto = function(idComponente){
-    let componente = document.getElementById(idComponente);
-    let valorIngresado = componente.value;
-    return valorIngresado;
-}
-
-recuperarInt= function(idComponente){
-    let valorCaja = recuperarTexto(idComponente);
-    valorEntero = parseInt(valorCaja);
-    return valorEntero;
-}
-
-recuperarFloat= function(idComponente){
-    let valorCaja = recuperarTexto(idComponente);
-    valorDecimal = parseFloat(valorCaja);
-    return valorDecimal;
-}
