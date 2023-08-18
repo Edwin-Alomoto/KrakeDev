17ec7625d8b2846a129432832fcddbfeb9d1c663
Edwin-Alomoto <Edwin_Alomoto@outlook.com>
2023-08-18 11:47:09 -0500

    Funciones recuperarInt recuperarFloat

--- utilitarios/saludos.js
+++ utilitarios/saludos.js
@@ -1,3 +1,12 @@
+saludar= function(){
+    // recuperar el valor de la caja de texto
+    let nombre = recuperarTexto("txtNombre");
+    let apellido = recuperarTexto("txtApellido");
+
+    let edad = recuperarInt("txtEdad");
+    let estatura = recuperarFloat("txtEstatura");
+}
+
 recuperarTexto = function(idComponente){
     let componente = document.getElementById(idComponente);
     let valorIngresado = componente.value;
@@ -5,8 +14,14 @@ recuperarTexto = function(idComponente){
     return valorIngresado;
 }
 
-saludar= function(){
-    // recuperar el valor de la caja de texto
-    let nombre = recuperarTexto("txtNombre");
-    let apellido = recuperarTexto("txtApellido");
+recuperarInt= function(idComponente){
+    let valorCaja = recuperarTexto(idComponente);
+    valorEntero = parseInt(valorCaja);
+    return valorEntero;
+}
+
+recuperarFloat= function(idComponente){
+    let valorCaja = recuperarTexto(idComponente);
+    valorDecimal = parseFloat(valorCaja);
+    return valorDecimal;
 }
