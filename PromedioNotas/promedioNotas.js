d214f9948ee1a1763419001c4714e54835786069
Edwin-Alomoto <Edwin_Alomoto@outlook.com>
2023-08-20 14:18:32 -0500

    Se agrego condiciones y ademas se le coloco gif

--- PromedioNotas/promedioNotas.js
+++ PromedioNotas/promedioNotas.js
@@ -1,8 +1,14 @@
 const calcularPromedioNotas = function(){
     let valorUno = recuperarFloat("txtNota1");
     let valorDos = recuperarFloat("txtNota2");
-    let valorTres = recuperarFloat("txtNota3") 
-    let promedioNota = calcularPromedio(valorUno,valorDos,valorTres);
-    let mensaje = ("El promedio es: "+promedioNota.toFixed(2));
+    let valorTres = recuperarFloat("txtNota3");
+    let promedioNota = (calcularPromedio(valorUno,valorDos,valorTres)).toFixed(2);
+    let mensaje = ("El promedio es: "+promedioNota);
     mostrarTexto("lblPromedio",mensaje);
+    
+    if(promedioNota>=7){
+        mostrarImagen("mostrarGif","Celebracion.gif");
+    }else{
+        mostrarImagen("mostrarGif","Fracaso.gif");
+    }
 }
