3e062a3c6e6d3480156240cb4118a860c0b0afbd
Edwin-Alomoto <Edwin_Alomoto@outlook.com>
2023-08-23 23:51:10 -0500

    Correccion del reto 33

--- condicionales/condicionales.js
+++ condicionales/condicionales.js
@@ -1,46 +1,52 @@
 const calcularTasaInteres = function (ingresoAnual) {
+  let ingresoAnualFloat = parseFloat(ingresoAnual);
   let ValorTasa;
-  if (ingresoAnual < 300000) {
+  if (ingresoAnualFloat < 300000) {
     ValorTasa = 0.16;
-  } else if (ingresoAnual >= 300000 && ingresoAnual < 500000) {
+  } else if (ingresoAnualFloat >= 300000 && ingresoAnualFloat < 500000) {
     ValorTasa = 0.15;
-  } else if (ingresoAnual >= 500000 && ingresoAnual < 1000000) {
+  } else if (ingresoAnualFloat >= 500000 && ingresoAnualFloat < 1000000) {
     ValorTasa = 0.14;
-  } else if (ingresoAnual >= 1000000 && ingresoAnual < 2000000) {
+  } else if (ingresoAnualFloat >= 1000000 && ingresoAnualFloat < 2000000) {
     ValorTasa = 0.13;
-  } else if (ingresoAnual >= 2000000) {
+  } else if (ingresoAnualFloat >= 2000000) {
     ValorTasa = 0.12;
   }
   return ValorTasa;
 }
 
 const calcularCapacidadPago = function (edad, ingresos, egresos) {
+  let edadInt = parseInt(edad);
+  let ingresoFloat = parseFloat(ingresos);
+  let egresoFloat = parseFloat(egresos);
   let valorCuota;
-  if (edad > 50) {
-    valorCuota = (egresos - ingresos) * 0.3;
-  } else if (edad <= 50 && edad >= 18) {
-    valorCuota = (egresos - ingresos) * 0.4;
+  if (edadInt > 50) {
+    valorCuota = (egresoFloat - ingresoFloat) * 0.3;
+  } else if (edadInt <= 50 && edadInt >= 18) {
+    valorCuota = (egresoFloat - ingresoFloat) * 0.4;
   }
   return valorCuota;
-};
+}
 
 const calcularDescuento = function (precio, cantidad) {
+  let precioFloat = parseFloat(precio);
+  let cantidadInt = parseInt(cantidad);
   let valorPagar;
   let subtotal;
   let valorDescuento;
-  if (cantidad < 3) {
-    subtotal = precio * cantidad;
+  if (cantidadInt < 3) {
+    subtotal = precioFloat * cantidadInt;
     valorPagar = subtotal;
-  } else if (cantidad >= 3 && cantidad >= 5) {
-    subtotal = precio - cantidad;
+  } else if (cantidadInt >= 3 && cantidadInt >= 5) {
+    subtotal = precioFloat - cantidadInt;
     valorDescuento = subtotal * 0.02;
     valorPagar = subtotal - valorDescuento;
-  } else if (cantidad >= 6 && cantidad >= 11) {
-    subtotal = precio - cantidad;
+  } else if (cantidadInt >= 6 && cantidadInt >= 11) {
+    subtotal = precioFloat - cantidadInt;
     valorDescuento = subtotal * 0.03;
     valorPagar = subtotal - valorDescuento;
-  } else if (cantidad >= 12) {
-    subtotal = precio - cantidad;
+  } else if (cantidadInt >= 12) {
+    subtotal = precioFloat - cantidadInt;
     valorDescuento = subtotal * 0.04;
     valorPagar = subtotal - valorDescuento;
   }
@@ -48,91 +54,98 @@ const calcularDescuento = function (precio, cantidad) {
 }
 
 const determinarColesterolLDL = function (nivelColesterol) {
-  let colesterol;
-  if (nivelColesterol < 100) {
-    colesterol = console.log(
-      `Su nivel den colesterol LDL es de ${nivelColesterol}. Optimo (lo mejor para sus salud)`);
-  } else if (nivelColesterol >= 100 && nivelColesterol <= 129) {
-    colesterol = console.log(`Su nivel den colesterol LDL es de ${nivelColesterol}. Casi optimo (mas o menos)`);
-  } else if (nivelColesterol >= 130 && nivelColesterol <= 159) {
-    colesterol = console.log(`Su nivel den colesterol LDL es de ${nivelColesterol}. Limite superior del rango normal`);
-  } else if (nivelColesterol >= 160 && nivelColesterol <= 189) {
-    colesterol = console.log(`Su nivel den colesterol LDL es de ${nivelColesterol}. NIVEL ALTO`);
-  } else if (nivelColesterol >= 190) {
-    colesterol = console.log(`Su nivel den colesterol LDL es de ${nivelColesterol}. NIVEL MUY ALTO`);
+  let nivelColesterolFloat = parseFloat(nivelColesterol);
+  let colesterolLDL;
+  if (nivelColesterolFloat < 100) {
+    colesterolLDL = console.log(
+      `Su nivel den colesterol LDL es de ${nivelColesterolFloat}. Optimo (lo mejor para sus salud)`
+    );
+  } else if (nivelColesterolFloat >= 100 && nivelColesterolFloat <= 129) {
+    colesterolLDL = console.log(
+      `Su nivel den colesterol LDL es de ${nivelColesterolFloat}. Casi optimo (mas o menos)`
+    );
+  } else if (nivelColesterolFloat >= 130 && nivelColesterolFloat <= 159) {
+    colesterolLDL = console.log(
+      `Su nivel den colesterol LDL es de ${nivelColesterolFloat}. Limite superior del rango normal`
+    );
+  } else if (nivelColesterolFloat >= 160 && nivelColesterolFloat <= 189) {
+    colesterolLDL = console.log(
+      `Su nivel den colesterol LDL es de ${nivelColesterolFloat}. NIVEL ALTO`
+    );
+  } else if (nivelColesterolFloat >= 190) {
+    colesterolLDL = console.log(
+      `Su nivel den colesterol LDL es de ${nivelColesterolFloat}. NIVEL MUY ALTO`
+    );
   }
-  return colesterol;
+  return colesterolLDL;
 }
 
 const validarClave = function (clave) {
-  let validaClave;
   if (clave.length < 8 && clave.length >= 16) {
-    validaClave = true;
+    return true;
   } else {
-    validaClave = false;
+    return false;
   }
-  return validaClave;
 }
 
 const esMayuscula = function (caracter) {
-  let mayuscula;
   let letra = caracter.charCodeAt(0);
   if (letra >= 65 && letra <= 90) {
-    mayuscula = true;
+    return true;
   } else {
-    mayuscula = false;
+    return false;
   }
-  return mayuscula;
 }
 
 const esMinuscula = function (caracter) {
-  let miniscula;
   let letra = caracter.charCodeAt(0);
   if (letra >= 97 && letra <= 122) {
-    miniscula = true;
+    return true;
   } else {
-    miniscula = false;
+    return false;
   }
-  return miniscula;
-};
-
+}
 const esDigito = function (caracter) {
-  let digito;
   let valorDigital = caracter.charCodeAt(0);
   if (valorDigital >= 48 && valorDigital <= 57) {
-    digito = true;
+    return true;
   } else {
-    digito = false;
+    return false;
   }
-  return digito;
-};
+}
 
 const darPermiso = function (notaMatematica, notaFisica, notaGeometria) {
-  let permiso;
-  if (notaMatematica > 90 || notaFisica > 90 || notaGeometria > 90) {
-    permiso = true;
+  let matematica = parseFloat(notaMatematica);
+  let fisica = parseFloat(notaFisica);
+  let geometria = parseFloat(notaGeometria);
+  if (matematica > 90 || fisica > 90 || geometria > 90) {
+    return true;
   } else {
-    permiso = false;
+    return false;
   }
-  return permiso;
 }
 
 const otorgarPermiso = function (notaMatematica, notaFisica, notaGeometria) {
-  let permiso;
-  if ((notaMatematica > 90 || notaFisica > 90) && notaGeometria > 80) {
-    permiso = true;
+  let matematica = parseFloat(notaMatematica);
+  let fisica = parseFloat(notaFisica);
+  let geometria = parseFloat(notaGeometria);
+  if ((matematica > 90 || fisica > 90) && geometria > 80) {
+    return true;
   } else {
-    permiso = false;
+    return false;
   }
-  return permiso;
 }
 
 const dejarSalir = function (notaMatematica, notaFisica, notaGeometria) {
-  let permiso;
-  if ((notaMatematica > 90 || notaFisica > 90 || notaGeometria > 90) && (notaFisica > notaMatematica)) {
-    permiso = true;
+  let matematica = parseFloat(notaMatematica);
+  let fisica = parseFloat(notaFisica);
+  let geometria = parseFloat(notaGeometria);
+  if (
+    (matematica > 90 || fisica > 90 || geometria > 90) &&
+    fisica > matematica
+  ) {
+    return true;
   } else {
-    permiso = false;
+    return false;
   }
-  return permiso;
 }
