303c4e671f5426fc40fd177820c7fe9fd7e8d85f
Edwin-Alomoto <Edwin_Alomoto@outlook.com>
2023-08-29 12:04:57 -0500

    reto 36 implementado

--- juego/juego.js
+++ juego/juego.js
@@ -1,25 +1,35 @@
-const jugar = function(seleccionado){
+let puntoUsuario = 0;
+let puntoMaquina = 0;
+const jugar = function (seleccionado) {
     let valorUsuario;
     let seleccionadoMaquina;
     let valorMaquina;
-
-    if(seleccionado==2){
-        cambiarImagen("imgUsuario","./Imagenes/papel.png");
+    let ganador;
+    if (seleccionado == 2) {
+        cambiarImagen("imgUsuario", "./Imagenes/papel.png");
         valorUsuario = 2;
         seleccionadoMaquina = generarElemento();
-        valorMaquina=generarRuta(seleccionadoMaquina);
-
-    }else if (seleccionado==1){
-        cambiarImagen("imgUsuario","./Imagenes/piedra.png");
+        valorMaquina = generarRuta(seleccionadoMaquina);
+        ganador = determinarGanador(valorUsuario, valorMaquina);
+    } else if (seleccionado == 1) {
+        cambiarImagen("imgUsuario", "./Imagenes/piedra.png");
         valorUsuario = 1;
         seleccionadoMaquina = generarElemento();
-        valorMaquina=generarRuta(seleccionadoMaquina);
-    }else if (seleccionado==3){
-        cambiarImagen("imgUsuario","./Imagenes/tijera.png"); 
+        valorMaquina = generarRuta(seleccionadoMaquina);
+        ganador = determinarGanador(valorUsuario, valorMaquina);
+    } else if (seleccionado == 3) {
+        cambiarImagen("imgUsuario", "./Imagenes/tijera.png");
         valorUsuario = 3;
         seleccionadoMaquina = generarElemento();
-        valorMaquina=generarRuta(seleccionadoMaquina);
+        valorMaquina = generarRuta(seleccionadoMaquina);
+        ganador = determinarGanador(valorUsuario, valorMaquina);
     }
 
-    determinarGanador(valorUsuario,valorMaquina);
+    if (ganador == "PERDISTE") {
+        puntoMaquina = puntoMaquina + 1;
+        cambiarTexto("lblResultadoMaquina", puntoMaquina);
+    } else if (ganador == "GANASTE") {
+        puntoUsuario = puntoUsuario + 1;
+        cambiarTexto("lblResultadoUsuario", puntoUsuario);
+    }
 }
