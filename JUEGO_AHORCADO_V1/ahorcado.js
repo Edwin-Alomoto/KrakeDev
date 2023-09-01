9e5e073846e9eb62f2511a8b4909df9bbb49d5e3
Edwin-Alomoto <Edwin_Alomoto@outlook.com>
2023-09-01 09:57:00 -0500

    reto 39 paso 1

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,4 +1,5 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta;
 const esMayuscula = function(caracter){
     if(caracter.charCodeAt(0)>=65 && caracter.charCodeAt(0)<=90){
         return true;
@@ -7,3 +8,36 @@ const esMayuscula = function(caracter){
     }
 
 }
+
+const guardarPalabra = function() {
+    let letra;
+    let contadorMayuscula=0;
+    let palabra = recuperarTexto("txtSecreta");
+    if(palabra.length==5){
+        for(let i=0;i<=palabra.length-1;i++){
+            letra = palabra.charAt(i);
+            if(esMayuscula(letra)){
+                contadorMayuscula = contadorMayuscula + 1;
+            }
+        }
+        if(contadorMayuscula!=5){
+            alert("todo las palabras debe ser mayuscula")
+        }else{
+            palabraSecreta=palabra;
+            console.log(palabraSecreta)
+        }
+    }
+
+
+
+
+
+
+
+
+    /*if(validoMayuscula){
+        var palabraSecreta =palabra;
+        console.log(palabraSecreta);
+    }else{*/
+       
+    }
