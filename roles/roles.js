36aa3e319c5542e3c6276dda5c37d257d307c7b5
Edwin-Alomoto <Edwin_Alomoto@outlook.com>
2023-09-04 10:00:04 -0500

    reto 46 implementado

--- roles/roles.js
+++ roles/roles.js
@@ -1,24 +1,55 @@
 let empleados = [
-    {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    { cedula: "1714616123", nombre: "John", apellido: "Cena", sueldo: 500.0 },
+    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 },
+    {cedula:"2445600234",nombre:"Jonathan",apellido:"Rivadeneira",sueldo:1200.0}
 ]
 
 //botones principales
-const mostrarOpcionEmpleado = function(){
+const mostrarOpcionEmpleado = function () {
+    mostrarEmpleados();
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
 }
 
-const mostrarOpcionRol = function(){
+const mostrarOpcionRol = function () {
     mostrarComponente("divRol");
     ocultarComponente("divEmpleado");
     ocultarComponente("divResumen");
 }
 
-const mostrarOpcionResumen = function(){
+const mostrarOpcionResumen = function () {
     mostrarComponente("divResumen");
     ocultarComponente("divEmpleado");
     ocultarComponente("divRol");
+}
+
+// Mostrar empleados tablas
+
+const mostrarEmpleados = function () {
+    let elementoEmpleados
+    let cmpTabla = document.getElementById("tablaEmpleados");
+    let contenidoTabla = "<table>" +
+
+        "<tr>" +
+        "<th>CEDULA</th>" +
+        "<th>NOMBRE</th>" +
+        "<th>APELLIDO</th>" +
+        "<th>SUELO</th>" +
+        "</tr>";
+
+        for(let i = 0; i < empleados.length; i++) {
+            elementoEmpleados = empleados[i];
+            contenidoTabla +=
+            "<tr>" +
+            "<td>" + elementoEmpleados.cedula + "</td>" +
+            "<td>" + elementoEmpleados.nombre + "</td>" +
+            "<td>" + elementoEmpleados.apellido + "</td>" +
+            "<td>" + elementoEmpleados.sueldo + "</td>" +
+            "</tr>";
+        }
+        
+    contenidoTabla += "</table>";
+    cmpTabla.innerHTML = contenidoTabla;
 }
 
