0a371f4471d4119301093cbf0d4ee2fd40275f15
Edwin-Alomoto <Edwin_Alomoto@outlook.com>
2023-09-19 18:48:40 -0500

    archivos de referencia

--- bucles/ejemplosFor.js
+++ bucles/ejemplosFor.js
@@ -1,46 +1,46 @@
 const mostrarNumeros = function () {
-    console.log("antes del for");
-    for (let i = 0; i < 4; i++) {
-        console.log(i);
-    }
-    console.log("despues del for");
-}
+  console.log("antes del for");
+  for (let i = 0; i < 4; i++) {
+    console.log(i);
+  }
+  console.log("despues del for");
+};
 
 const mostrarNumeros2 = function () {
-    console.log("antes del for");
-    for (let indice = 1; indice <= 5; indice++) {
-        console.log(indice);
-    }
-    console.log("despues del for");
-}
+  console.log("antes del for");
+  for (let indice = 1; indice <= 5; indice++) {
+    console.log(indice);
+  }
+  console.log("despues del for");
+};
 
 const mostrarNumeros3 = function () {
-    console.log("antes del for");
-    for (let a = 2; a <= 10; a += 2) {
-        console.log(a);
-    }
-    console.log("despues del for");
-}
+  console.log("antes del for");
+  for (let a = 2; a <= 10; a += 2) {
+    console.log(a);
+  }
+  console.log("despues del for");
+};
 
 const mostrarNumeros4 = function () {
-    console.log("antes del for");
-    for (let a = 10; a >= 2; a -= 2) {
-        console.log(a);
-    }
-    console.log("despues del for");
-}
+  console.log("antes del for");
+  for (let a = 10; a >= 2; a -= 2) {
+    console.log(a);
+  }
+  console.log("despues del for");
+};
 
 const hackearNasaPelis = function () {
-    for (let a = 0; a <= 100; a += 10) {
-        console.log(`hackeando la nasa :${a}%`);
-    }
-    console.log("La nasa a sido hackeada");
-}
+  for (let a = 0; a <= 100; a += 10) {
+    console.log(`hackeando la nasa :${a}%`);
+  }
+  console.log("La nasa a sido hackeada");
+};
 
 const mostrarImpares = function () {
-    console.log("Antes for");
-    for (let a = 1; a <= 21; a+=2) {
-        console.log(a);
-    }
-    console.log("Despues for");
-}
+  console.log("Antes for");
+  for (let a = 1; a <= 21; a += 2) {
+    console.log(a);
+  }
+  console.log("Despues for");
+};
